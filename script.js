4a777666d2ef92663b861a8d1e1fc97ab04469f7
JayanakaWijethunga <jayanaka987@gmail.com>
2019-11-15 07:21:21 -0800

    basic face recognition

--- script.js
+++ script.js
@@ -14,14 +14,31 @@ faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 
 
 function start(){
+
+    const container=document.createElement('div');
+    container.style.position = 'relative';
+    document.body.append(container);
     document.body.append('Loaded');
     getFile.addEventListener('change', async() => {
 
       const image=await faceapi.bufferToImage(getFile.files[0])
-      document.body.append(image)
+      container.append(image)
+      
+      
+      const canvas = faceapi.createCanvasFromMedia(image)
+      container.append(canvas)
+      const displaySize = {width:image.width,height:image.height}
+      faceapi.matchDimensions(canvas,displaySize)
       const detection=await faceapi.detectAllFaces(image)
       .withFaceLandmarks().withFaceDescriptors()
-      document.body.append(detection.length)
+      const resizeDetections = faceapi.resizeResults(detection,displaySize)
+      resizeDetections.forEach(detection => {
+            const box = detection.detection.box
+            const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face'})
+            drawBox.draw(canvas)
+
+
+      })
 
     })
 }
